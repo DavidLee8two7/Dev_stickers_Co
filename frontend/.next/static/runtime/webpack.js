1e015a2c05aef0bd68e24029a430e485f9c9c995
David Lee <DavidLee8two7@gmail.com>
2018-10-29 16:46:09 -0700

    using react with graphql: format.js & ternary to &&

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "262914ce7aeaf66121d2";
+/******/ 	var hotCurrentHash = "8c0f3f573341850ed833";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
