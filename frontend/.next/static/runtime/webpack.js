dfcaa92910435e3e2c8730debcf739138af3d403
David Lee <DavidLee8two7@gmail.com>
2018-11-14 16:37:36 -0800

    frontend - Displaying single items

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1121e5051c9a715764a5";
+/******/ 	var hotCurrentHash = "6d53ae86d67d5c5faa79";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
