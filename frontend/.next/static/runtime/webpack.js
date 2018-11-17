c7e1feca7df9b2c5da3920fc6f8428ea52fbfeae
David Lee <DavidLee8two7@gmail.com>
2018-11-16 17:39:16 -0800

    Permissions management

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9e5aafd5b098ddb4eb2e";
+/******/ 	var hotCurrentHash = "9d753788fbc051b3ef08";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
