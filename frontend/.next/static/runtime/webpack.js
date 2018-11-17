587d1fd0c1db34c9bd104d0f01228a89ae83643f
David Lee <DavidLee8two7@gmail.com>
2018-11-17 01:26:41 -0800

    Apollo local state queries and mutations

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02fd788918284b6db296";
+/******/ 	var hotCurrentHash = "58b815a93207746b7970";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
